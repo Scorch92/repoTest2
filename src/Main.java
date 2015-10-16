52ba60b9dfef50eb9d6f690949291f7f74f096e3
Gautier Julien <scorch92@hotmail.be>
2015-10-16 15:55:34 +0200

    Factorielle itérative fonctionnelle

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,27 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		
+		factoI(10);
 	}
 	
-	public void factoI(int n)
+	public static void factoI(int n)
 	{
+		int i;
+		int nbr = 2;
 		
+		if(n == 0)
+		{
+			nbr = 1;
+		}
+		else
+		{
+			for(i = 1; i < n; i++)
+			{
+				nbr = nbr * 2;
+			}
+		}
+		
+		System.out.println(nbr);
 	}
 	
 	public void factoR(int n)
