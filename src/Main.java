6256e914ef8af15c27c58d11269e7836546f3ee1
Gautier Julien <scorch92@hotmail.be>
2015-10-16 16:25:24 +0200

    Merge2

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,12 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		factoI(10);
+		
 	}
 	
 	public static void factoI(int n)
 	{
-		int i;
-		int nbr = 2;
 		
-		if(n == 0)
-		{
-			nbr = 1;
-		}
-		else
-		{
-			for(i = 1; i < n; i++)
-			{
-				nbr = nbr * 2;
-			}
-		}
-		
-		System.out.println(nbr);
 	}
 	
 	public void factoR(int n)
