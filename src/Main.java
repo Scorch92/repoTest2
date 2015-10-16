8ac047e6c5a8162c235ab4d7e21237c2da3514b3
Gautier Julien <scorch92@hotmail.be>
2015-10-16 16:35:05 +0200

    Coucou

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,12 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
-		
-=======
 		factoR(5);
->>>>>>> 0f4c2c65f59fde7334fa3c766799fcc8ecb36a35
 	}
 	
 	public static void factoI(int n)
 	{
-		
+		System.out.println("Bonjour");
 	}
 	
 	public static void factoR(int n)
